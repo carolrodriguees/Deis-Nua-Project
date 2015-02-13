db7035dd9850c41572e9cf4ae063d558dcde4b3d
Carol Rodrigues <caahroriguees28@gmail.com>
2015-02-13 20:54:42 -0200

    Update 01-Fate-Intro.py

--- 01-Fate-Intro.py
+++ 01-Fate-Intro.py
@@ -1,42 +1,42 @@
-Foi um impulso como um relâmpago. 
-A ponta da lança empurrou para furar meu coração.
-Seria inútil tentar me esquivar.
-Sendo luz, é invisível ao olho humano.
+Foi um impulso como um relâmpago.
+Uma ponta de lança para furar meu coração.
+Tentar se esquivar seria inútil.
+A iluminação, que é invisível ao olho humano.
 
-Mas...
-O raio que tenta me furar...
-....É repelido pela luz da lua que tenta me salvar.
+Mas ...
+O raio que tenta me furar ...
+... é repelido pela luz da lua que tenta me salvar.
 Clang, um som bonito.
 
-Não, o som antes de mim é mais pesado do que o aço.
-A armadura que ela está usando não é bonito em tudo e não refinado como a noite fria.
-O som não era bonito em tudo.
+Não, o som atrás de mim é mais pesado do que o aço.
+A armadura que ela está usando não é bonita e tão impura como a noite fria.
+O som não era bonito.
 
 Na verdade, foi o som de aço.
-É justo que o cavaleiro é bonito o suficiente para transformá-lo em um som encantador como um sino.
+É justo que o cavaleiro seja bonito o suficiente para transformá-lo em um som encantador como um sino.
 
-"_______ Eu lhe pergunto. É você o meu Mestre?" 
+"__Eu lhe peço. Você é meu mestre? " 
 
-Ela pergunta em uma voz que ilumina a escuridão.
+Ela pergunta com uma voz que ilumina a escuridão.
 
-"Eu vim adiante em resposta à sua invocação.
-A partir deste momento em diante, minha espada estará com você e seu destino estará comigo. Agora, o nosso contrato está completo."
-
-Sim, o contrato foi concluído.
-Quando ela me escolheu como seu Mestre...
-Eu tenho certeza que eu jurei ajudá-la também.
-O luar ainda ilumina a escuridão.
-Como se seguindo o exemplo do cavaleiro, o galpão fica em silêncio novamente.
+"Eu vim em resposta ao seu convite. A partir deste momento, minha espada estará com você e seu destino estará comigo. 
+Agora, o nosso contrato está completo. "
+Sim,o contrato foi concluído.
+Quando ela me escolheu como seu mestre...
+Tenho certeza que jurei ajudá-la também.
+O luar ainda ilumia a escuridão.
+Como que seguindo o exemplo do cavaleiro,o galpão volta a ficar em silêncio.
 O tempo parou.
 A cena dura menos de um segundo.
 
 Mas...
-Tenho certeza de que vou me lembrar dessa cena vividamente mesmo quando eu for para o inferno.
+Tenho certeza de que vou me lembrar disso vividamente mesmo quando eu for para o inferno.
 
 O rosto ligeiramente virado.
 Os tranquilos olhos verdes.
-No instante em que se torna uma eternidade.
+O instante se torna uma eternidade.
 O equipamento azul simbolizando seus balanços no vento.
 
-_______ Uma leve luz azul filtra. 
+_______ Uma fraca luz azul entra.
 O cabelo dourado brilha à luz do luar.
+
